9b24813dfa3f90c0b07ee4bb315de585026549b0
plohkoon <greg.huber@outlook.com>
2019-04-27 12:46:39 -0700

    added the code for the first challenge as well as CSS to highlight code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './prism.css';
+
+let sumOfThreeFive = (maxVal)=>{
+  const nums = [3,5,6,9,10,12,15],
+        div = Math.floor(maxVal / 15),
+        mod = maxVal % 15;
+
+  let   sum = 0;
+
+  for(let i = 1; i < div + 1; i++) {
+      sum += (i * 60);
+  }
+
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] <= mod) {
+      sum += nums[i];
+    }
+    else {
+      break;
+    }
+  }
+
+  return sum;
+}
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      number: 5
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <pre>
-            <code>
-              {"let x=5;\nlet y=7;"}
-            </code>
-          </pre>
-        </header>
+        <input type="string" value={this.state.number} onChange={(event) => {this.setState({number: event.target.value})}}></input>
+        <p>{sumOfThreeFive(this.state.number)}</p>
+        <pre className="language-js">
+          <code className="language-js">
+{`(maxVal)=>{
+  const nums = [3,5,6,9,10,12,15]
+        div = Math.floor(top / 15),
+        mod = top % 15;
+
+  let   sum = 0;
+
+  for(let i = 1; i < div + 1; i++) {
+      sum += (i * 60);
+  }
+
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] <= mod) {
+      sum += nums[i];
+    }
+    else {
+      break;
+    }
+  }
+
+  return sum;
+}`}
+          </code>
+        </pre>
       </div>
     );
   }
